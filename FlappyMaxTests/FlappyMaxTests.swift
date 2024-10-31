58a563ef9a058feed18262d0f6078d8e4fb274e1
John Zheng <bluevisor@gmail.com>
2024-10-31 07:33:12 -0700

    Initial Commit

---
+++ FlappyMaxTests/FlappyMaxTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyMaxTests.swift
+//  FlappyMaxTests
+//
+//  Created by John Zheng on 10/31/24.
+//
+
+import Testing
+@testable import FlappyMax
+
+struct FlappyMaxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
